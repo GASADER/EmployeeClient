8d1688d99a8089912a86575113921435a5c151b3
josave <josave@odds.team>
2023-08-11 09:58:37 +0700

    Add: component navber

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
+import { NavberComponent } from './components/navber/navber.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavberComponent
   ],
   imports: [
     BrowserModule,
