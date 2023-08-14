acbbdd9f9d37f43ca2ed8dbd5e1a08685e490773
josave <josave@odds.team>
2023-08-14 16:21:22 +0700

    Feat: connect employee APIs

--- src/app/components/employee-dashboard-component/employee-dashboard-component.component.ts
+++ src/app/components/employee-dashboard-component/employee-dashboard-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EmployeeDataService } from 'src/app/employee-data.service';
+import { Employee } from 'src/app/employee';
 @Component({
   selector: 'app-employee-dashboard-component',
   templateUrl: './employee-dashboard-component.component.html',
   styleUrls: ['./employee-dashboard-component.component.css']
 })
-export class EmployeeDashboardComponentComponent {
+export class EmployeeDashboardComponentComponent implements OnInit {
 
+    employees: Employee[] = []
+
+    constructor(private employeeService: EmployeeDataService){}
+
+    ngOnInit(): void {
+        this.employeeService.gatAll().subscribe((employees) => {
+          this.employees = employees
+          console.log(this.employees)
+        })
+    }
 }
