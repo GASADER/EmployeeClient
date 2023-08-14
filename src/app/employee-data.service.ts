5782e5b1c5d3eff3204ac2e8bac22ce528144914
josave <josave@odds.team>
2023-08-14 15:38:19 +0700

    Feat: gatAllEmployee service

--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './EMPLOYEE';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeDataService {
+  private employeeAPIs = 'https://run.mocky.io/v3/89e9f836-2aa2-4779-9556-4bff31fbab49'
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  gatAll(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(this.employeeAPIs)
+  }
 }
