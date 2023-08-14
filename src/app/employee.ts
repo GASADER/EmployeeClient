a3446a475007b13215bdc1e87458102b429640ba
josave <josave@odds.team>
2023-08-14 15:30:46 +0700

    Add: employee interface

---
+++ src/app/employee.ts
@@ -0,0 +1,14 @@
+export interface Employee{
+    id: number,
+    uid: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    username: string,
+    email: string,
+    avatar: string,
+    gender: string,
+    phone_number: string,
+    wallet: 178995726,
+    date_of_birth: string
+}
