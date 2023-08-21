c8afac1dae756274e40b480f4f6fd217621f8d7f
josave <josave@odds.team>
2023-08-21 23:30:55 +0700

    Fix: api endpoint

--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -7,7 +7,7 @@ import { Employee } from './employee';
 })
 export class EmployeeDataService {
   private employeeAPIs = 'https://run.mocky.io/v3/89e9f836-2aa2-4779-9556-4bff31fbab49'
-
+  private SECEmployeeAPI = ': http://localhost:5152/api/employee'
 
   constructor(private http: HttpClient) { }
 
@@ -25,6 +25,6 @@ export class EmployeeDataService {
     return allEmployee;
   }
   createEmployee(employee:Employee) : Observable<Employee>{
-    return this.http.post<Employee>(this.employeeAPIs,employee,this.httpOptions)
+    return this.http.post<Employee>(this.SECEmployeeAPI,employee,this.httpOptions)
   }
 }
