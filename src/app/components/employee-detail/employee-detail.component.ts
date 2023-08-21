3ac4172baa5de1ed88f48887508156fe5db6afd4
josave <josave@odds.team>
2023-08-21 23:24:16 +0700

    Add: createEmployee functions

--- src/app/components/employee-detail/employee-detail.component.ts
+++ src/app/components/employee-detail/employee-detail.component.ts
@@ -1,28 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/employee';
+import { EmployeeDataService } from 'src/app/employee-data.service';
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
-  styleUrls: ['./employee-detail.component.css']
+  styleUrls: ['./employee-detail.component.css'],
 })
 export class EmployeeDetailComponent implements OnInit {
-  editFrom !: FormGroup;
+  @Input() employee?: Employee;
+  editFrom!: FormGroup;
+  
 
-  constructor(private router: Router, private fb: FormBuilder){}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private employeeDataService: EmployeeDataService
+  ) {}
 
   ngOnInit(): void {
-      this.editFrom = this.fb.group({
-        first_name: "",
-        last_name: "",
-        username: "",
-        email:""
-      })
+    this.getEditEmployee()
+    console.log(this.employee);
   }
-  onSubmit(){
-      const editFromData = this.editFrom.value;
-      console.log(editFromData);
-      this.router.navigate(['dashboard']);
+
+  getEditEmployee() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.employeeDataService.getByID(id).subscribe((e)=>(
+      this.employee = e
+    ));
+    this.editFrom = this.fb.group({
+      first_name: `${this.employee?.first_name}`,
+      last_name: `${this.employee?.last_name}`,
+      username: `${this.employee?.username}`,
+      email: `${this.employee?.email}`,
+    });
+  }
+
+  onSubmit() {
+    const editFromData = this.editFrom.value;
+    console.log(editFromData);
+    this.router.navigate(['dashboard']);
   }
 }
--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 import { Employee } from './employee';
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,20 @@ export class EmployeeDataService {
 
   constructor(private http: HttpClient) { }
 
-  gatAll(): Observable<Employee[]>{
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getAll(): Observable<Employee[]>{
     return this.http.get<Employee[]>(this.employeeAPIs)
+  }
+
+  getByID(id: number): Observable<Employee | undefined>{
+    const allEmployee = this.http.get<Employee[]>(this.employeeAPIs).pipe(map(e=> e.find(e=>e.id === id)))
+    console.log(allEmployee);
+    return allEmployee;
+  }
+  createEmployee(employee:Employee) : Observable<Employee>{
+    return this.http.post<Employee>(this.employeeAPIs,employee,this.httpOptions)
   }
 }
