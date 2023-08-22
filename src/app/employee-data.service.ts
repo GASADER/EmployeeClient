afee1c184086fa135d28324a02269759cf4f8c69
josave <josave@odds.team>
2023-08-22 07:23:02 +0700

    Fix: change endpoint

--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -7,7 +7,7 @@ import { Employee } from './employee';
 })
 export class EmployeeDataService {
   private employeeAPIs = 'https://run.mocky.io/v3/89e9f836-2aa2-4779-9556-4bff31fbab49'
-  private SECEmployeeAPI = ': http://localhost:5152/api/employee'
+  private SECEmployeeAPI = ': http://localhost:5292/api/employee'
 
   constructor(private http: HttpClient) { }
 
